31e95705fdea6ea81e8e0dee6832c68ecf9cdb39
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-05-26 15:25:51 +0530

    [Vinayak] Add: Selection Practice problems with if and else added

---
+++ JS/FlipCoin.js
@@ -0,0 +1,10 @@
+let coin_flipped = Math.floor((Math.random() * 10) % 2);
+let Heads = 0;
+let Tails = 1;
+
+if(coin_flipped == 0) {
+    console.log("HEADS");
+}
+else {
+    consolelog("TAILS");
+}
