ced18f36a1a8ef1e2ba85796ab34ad65823d4a5d
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-05-26 23:29:57 +0530

    [Vinayak] Add: JavaScript program using case statement is added

---
+++ CaseStatement/WeekDays.js
@@ -0,0 +1,27 @@
+let number4 = Math.floor(1 + ((Math.random() * 10) % 7));
+console.log("number entered: " + number4);
+switch (number4) {
+  case 1:
+    console.log("SUNDAY");
+    break;
+  case 2:
+    console.log("TUESDAY");
+    break;
+  case 3:
+    console.log("WEDNESDAY");
+    break;
+  case 4:
+    console.log("THURSDAY");
+    break;
+  case 5:
+    console.log("FRIDAY");
+    break;
+  case 6:
+    console.log("SATURDAY");
+    break;
+  case 7:
+    console.log("MONDAY");
+    break;
+  default:
+    console.log("Invalid!");
+}
