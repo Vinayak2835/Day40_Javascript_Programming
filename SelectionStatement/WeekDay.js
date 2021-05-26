9f7971a58bc9bbfb7412ff4c39c2bc282e224df2
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-05-26 22:43:27 +0530

    [Vinayak] Add: javascript program with selection statement is added

---
+++ SelectionStatement/WeekDay.js
@@ -0,0 +1,20 @@
+let number = Math.floor(1 + Math.random() * 10);
+console.log("number entered: " + number);
+
+if (number == 1) {
+  console.log("SUNDDAY");
+} else if (number == 2) {
+  console.log("MONDAY");
+} else if (number == 3) {
+  console.log("TUESDAY");
+} else if (number == 4) {
+  console.log("WEDNESDAY");
+} else if (number == 5) {
+  console.log("THURSDAY");
+} else if (number == 6) {
+  console.log("FRIDAY");
+} else if (number == 7) {
+  console.log("SATURDAY");
+} else {
+  console.log("Unknown!");
+}
