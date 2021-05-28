1c4301224fb6c6ce6ee65188eea258a2494db54b
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-05-28 23:29:08 +0530

    [Vinayak] Add: JavaScript program for Power of a number is added

--- UsingWhileLoop/Powers.js
+++ UsingWhileLoop/Powers.js
@@ -2,15 +2,11 @@ const readline6 = require("readline").createInterface({
     input: process.stdin,
     output:process.stdout
 });
-  
-  let powers1 = 1;
-  
-  readline6.question("Enter a number", (number) => {
+let powers1 = 1;readline6.question("Enter a number", (number) => {
     let result = 1;
       while(result < 128) {
            result = result * number;
            console.log(result);
       }
-        
-    readline6.close();
+      readline6.close();
   });
