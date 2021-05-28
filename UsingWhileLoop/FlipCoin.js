144281d3e1bb7709857911f0f617f789a1cfac3a
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-05-28 23:30:21 +0530

    [Vinayak] Add: Javascript program for FlipCoin is added

--- UsingWhileLoop/FlipCoin.js
+++ UsingWhileLoop/FlipCoin.js
@@ -5,15 +5,12 @@ let heads = 0;
 let tails = 0;
 while (heads != 11 && tails != 11) {
     var coin = Math.floor(Math.random()*10) % 2;
-
     if(coin == head) {
         heads++;
     } else {
         tails++;
     }
-
 }
-
     if(heads == 11) {
         console.log("Head Wins! " + heads);
     } else {
